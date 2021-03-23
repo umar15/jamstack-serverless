1626d3e3a60792d4a64c80a7f87af07de7d1c00a
Umar Khalid <umar.qur105@gmail.com>
2021-03-23 20:50:24 +0500

    step6 updated

--- step06_programmatically_create_pages/gatsby-node.js
+++ step06_programmatically_create_pages/gatsby-node.js
@@ -1,13 +1,10 @@
-exports.createPages = async function ({ actions}) {
-
-    actions.createPage({
-        path: "my-dynamic-page",
-        component: require.resolve(`./src/templates/dynamic-page.tsx`),
-        context: { 
-            // Data passed to context is available
-            // in pageContext props of the template component
-            name: "Zia",
-         },
-    });
-    console.log("End of Gatsby Node File");
-}
+exports.createPages = async ({ actions }) => {
+	actions.createPage({
+		path: "my-dynamic-page",
+		component: require.resolve(`./src/templates/dynamic-page.tsx`),
+		context: {
+			fname: "Umar",
+			lname: "Khalid",
+		},
+	});
+};
