edfdbb55a5580f08a6a623521fc81c70c03f1669
Umar Khalid <umar.qur105@gmail.com>
2021-03-23 21:40:32 +0500

    step7 updated

--- step07_client_only_routes/gatsby-node.js
+++ step07_client_only_routes/gatsby-node.js
@@ -1,15 +1,24 @@
 // Implement the Gatsby API “onCreatePage”. This is
 // called after every page is created.
-exports.onCreatePage = async ({ page, actions }) => {
-    const { createPage } = actions
-  
-    // Only update the `/app` page.
-    if (page.path.match(/^\/app/)) {
-      // page.matchPath is a special key that's used for matching pages
-      // with corresponding routes only on the client.
-      page.matchPath = "/app/*"
-  
-      // Update the page.
-      createPage(page)
-    }
-  }
+// exports.onCreatePage = async ({ page, actions }) => {
+// 	const { createPage } = actions;
+
+// 	// Only update the `/app` page.
+// 	if (page.path.match(/^\/app/)) {
+// 		// page.matchPath is a special key that's used for matching pages
+// 		// with corresponding routes only on the client.
+// 		page.matchPath = "/app/*";
+
+// 		// Update the page.
+// 		createPage(page);
+// 	}
+// };
+
+exports.onCreatePage = async ({ actions, page }) => {
+	const { createPage } = actions;
+	if (page.path.match(/^\/app/)) {
+		page.matchPath = "/app/*";
+	}
+
+	createPage(page);
+};
